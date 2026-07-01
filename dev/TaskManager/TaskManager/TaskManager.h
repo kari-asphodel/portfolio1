9ebab31ed579a3a34d2cfc1f1fe0eaa428f1d268
Kari Asphodel <124915307+kari-asphodel@users.noreply.github.com>
2026-07-01 14:16:51 -0400

    Added to TaskManager included add tasks, view tasks, and vector of tasks

--- dev/TaskManager/TaskManager/TaskManager.h
+++ dev/TaskManager/TaskManager/TaskManager.h
@@ -1,4 +1,13 @@
 #pragma once
+#include "Task.h"
+#include <vector>
+#include <string>
 class TaskManager
-{};
+{
+private:
+	std::vector<Task> tasks;
+public:
+	void AddTask(std::string title);
+	void ViewTasks() const;
+};
 
