37510d7ea02d87bd2b69d25a24c4399fc23472f2
Kari Asphodel <124915307+kari-asphodel@users.noreply.github.com>
2026-07-15 13:34:53 -0400

    finished task manager with adding filtering and sorting

--- dev/TaskManager/TaskManager/TaskManager.h
+++ dev/TaskManager/TaskManager/TaskManager.h
@@ -7,12 +7,17 @@ class TaskManager
 private:
 	std::vector<Task> activeTasks;
 	std::vector<Task> completedTasks;
+
+	void DisplayTaskList(const std::vector<Task>& taskList, std::string heading) const;
 public:
-	void AddTask(std::string title);
+	void AddTask(std::string title, std::string priority, std::string category);
 	void ViewActiveTasks() const;
 	void ViewCompletedTasks() const;
+	void ViewTasksByPriorty(std::string priority) const;
 
 	void CompleteTask(int index);
+	void SortActiveTasksByPriority();
+
 	void DisplaySummary() const;
 	int GetActiveTaskCount() const;
 };
